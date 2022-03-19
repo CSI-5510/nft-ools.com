1a1be21947235574056048de4c54366119ba3424
Sal Trupiano <43245129+saltrupiao@users.noreply.github.com>
2022-03-19 14:04:26 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,14 @@ switch ($GLOBALS['url_loc'][1])
 {
     case "/":
     break;
+    case "/item":
+        $BACKEND = "item";
+        $PAGE_TITLE = "item";
+        $FRONTEND = "item";
+    break;
     default:
         $BACKEND = "index";
-		$PAGE_TITLE = "Index";
+	$PAGE_TITLE = "Index";
         $FRONTEND = "index";
 	break;
 }
