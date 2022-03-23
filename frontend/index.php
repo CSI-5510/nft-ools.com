4415385eb8ce4f1386cdc220a7b6a52c2b25778f
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-03-22 20:18:19 -0400

    Confirming tailwindcss library and showing flexbox

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,39 @@
+<?php 
+?>
+
 <!-- navbar -->
 <h1>DB Test result:</h1>
 <p><?php> echo $result; </?></p>
+<header>
+Displayed from frontend/index.php
+
+<!--
+Demonstrating flex box using TailwindCSS that is referenced through a CDN inside root/public_html/head.php!!!
+-->
+
+<!-- Parent Container -->
+<div>
+
+	<!-- Row One Container -->
+	<div class="flex justify-center p-6 space-x-3 bg-white">
+		<!-- Row One Item One -->
+		<div class="w-24 h-24 bg-gray-100 ">
+		item
+		</div>
+		<!-- Row One Item Two -->	
+		<div class="w-24 h-24 bg-gray-100 ">
+		item
+		</div>
+		<!-- Row One Item Three -->	
+		<div class="w-24 h-24 bg-gray-100 ">
+		item
+		</div>
+	</div>
+	<!-- Row Container End -->
+	
+	
+	
+</div>
+<!-- Parent Container End -->
+
+</header>
