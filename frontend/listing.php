71962786c9f0834033f0620e0a4bb5cd19b0d5c4
jrmachado <jrmachado@electro-matic.com>
2022-04-01 08:35:26 -0400

    switching to url

--- frontend/listing.php
+++ frontend/listing.php
@@ -2,62 +2,59 @@
     // FRONTEND
 ?>
 
-<div class="flex flex-row justify-between items-center">
-    <div id="listing_title_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+<div class="flex flex-row items-center">
+    <div id="listing_title_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for listing title
     </div>
-    <div id="listing_title_input" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="listing_title_input" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for listing title
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="item_description_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+    <div id="item_description_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for item description
     </div>
-    <div id="item_description" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="item_description" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for item descriptions
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="item_image_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+    <div id="item_image_label" cclass="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for item image
     </div>
-    <div id="item_image" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="item_image" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for item image (drag and drop)
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="documentation_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+    <div id="documentation_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for item documentation
     </div>
-    <div id="documentation_file_staging" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="documentation_file_staging" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for item documentation (drag and drop)
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="receipt_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+    <div id="receipt_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for item receipt
     </div>
-    <div id="receipt_image" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="receipt_image" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for item receipt (drag and drop)
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="affidavit_of_quality_label" class="p-4 m-10 w-3/5 bg-green-100 text-center">
+    <div id="affidavit_of_quality_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for affidavit of quality
     </div>
-    <div id="affidavit_of_quality_container" class="p-4 m-10 w-1/5 bg-green-100 text-center">
+    <div id="affidavit_of_quality_container" class="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         input field for affidavit of quality (drag and drop)
     </div>
 </div>
 <div class="flex flex-row justify-between items-center">
-    <div id="price_label" class="p-4 m-10 bg-green-100 text-center">
+    <div id="price_label" class="p-4 m-10 ml-10 mr-5 w-1/3 bg-green-100 text-center">
         label for price
     </div>
-    <div id="price_value" class="p-4 m-10 bg-green-100 text-center">
+    <div id="price_value" cclass="p-4 m-10 ml-5 mr-10 w-2/3 bg-green-100 text-center">
         price from algorithm
     </div>
-</div>
-<div id="lineage" class="p-4 m-10 bg-green-100 text-center">
-    lineage
 </div>
