2b9b99d0d9b1a6f6669915e6aa7cb75d28c3506c
jrmachado <jrmachado@electro-matic.com>
2022-04-06 13:31:18 -0400

    add_item html to function

--- frontend/add_item.php
+++ frontend/add_item.php
@@ -2,66 +2,6 @@
     // FRONTEND
     include('../functions/functions.add_item.php');
     include_once('../constants/constants.all.php');
-?>
 
-<form method="POST" action="/<?php echo $GLOBALS['url_loc'][0];?>/collector/add_item" enctype="multipart/form-data">
-    <div class="flex flex-row items-start">
-        <?php
-            drawLabel('TITLE', $LISTING_LABEL);
-            drawTextInput($ADD_ITEM_TITLE, $LISTING_INPUT_AREA);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('CATEGORY', $LISTING_LABEL);
-            drawSelectOption($ADD_ITEM_CATEGORY, $LISTING_INPUT_AREA, $options);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('DESCRIPTION', $LISTING_LABEL);
-            drawTextArea($ADD_ITEM_DESCRIPTION, $LISTING_INPUT_AREA);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('IMAGE', $LISTING_LABEL);
-            drawFileUpload($ADD_ITEM_IMAGE, $ADD_ITEM_IMAGE, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('DOCUMENTATION', $LISTING_LABEL);
-            drawFileUpload($ADD_ITEM_DOCUMENTATION, $ADD_ITEM_DOCUMENTATION, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('RECEIPT', $LISTING_LABEL);
-            drawFileUpload($ADD_ITEM_RECEIPT, $ADD_ITEM_RECEIPT, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('PURCHASE DATE', $LISTING_LABEL);
-            drawDateInput($ADD_ITEM_ORIGINAL_PURCHASE_DATE, $LISTING_INPUT_AREA);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('PURCHASE PRICE', $LISTING_LABEL);
-            drawTextInput($ADD_ITEM_ORIGINAL_PURCHASE_PRICE, $LISTING_INPUT_AREA);
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawLabel('AFFIDAVIT OF QUALITY', $LISTING_LABEL);
-            drawAffidavit($ADD_ITEM_AFFIDAVIT_NAME, $ADD_ITEM_AFFIDAVIT_VERIFY, 'i gaurantee that this thing works...');
-        ?>
-    </div>
-    <div class="flex flex-row justify-between items-start">
-        <?php
-            drawSubmitButton($BLUE_BUTTON." mx-40 my-10 w-full");
-        ?>
-    </div>
-</form>
+    drawAddItem();
+?>
--- functions/functions.add_item.php
+++ functions/functions.add_item.php
@@ -27,4 +27,91 @@
         return;
     }
 
+    
+    /** draws add item page
+     *
+     * @return void draws to page
+     */
+    function drawAddItem(){
+
+        // CONSTANTS
+        include('../constants/constants.all.php');
+        $OPEN_ROW = '<div class="'.$FLEX_ROW_JUSTIFY.'">';
+        $CLOSE_ROW = '</div>';
+        
+        // OPEN FORM
+        echo '<form method="POST" action="/'.$GLOBALS["url_loc"][0].'/collector/add_item" enctype="multipart/form-data">';
+        
+        // ROW TITLE
+        echo $OPEN_ROW;
+        drawLabel('TITLE', $LISTING_LABEL);
+        drawTextInput($ADD_ITEM_TITLE, $LISTING_INPUT_AREA);
+        echo $CLOSE_ROW;
+        
+        // ROW CATEGORY
+        echo $OPEN_ROW;
+        drawLabel('CATEGORY', $LISTING_LABEL);
+        drawSelectOption($ADD_ITEM_CATEGORY, $LISTING_INPUT_AREA, $options);
+        echo $CLOSE_ROW;
+        
+        // ROW SERIAL NUMBER
+        echo $OPEN_ROW;
+        drawLabel('SERIAL NUMBER', $LISTING_LABEL);
+        drawTextInput($ADD_ITEM_SERIAL_NUMBER, $LISTING_INPUT_AREA);
+        echo $CLOSE_ROW;
+
+        // ROW DESCRIPTION
+        echo $OPEN_ROW;
+        drawLabel('DESCRIPTION', $LISTING_LABEL);
+        drawTextArea($ADD_ITEM_DESCRIPTION, $LISTING_INPUT_AREA);
+        echo $CLOSE_ROW;
+        
+        // ROW IMAGE
+        echo $OPEN_ROW;
+        drawLabel('IMAGE', $LISTING_LABEL);
+        drawFileUpload($ADD_ITEM_IMAGE, $ADD_ITEM_IMAGE, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
+        echo $CLOSE_ROW;
+        
+        // ROW DOCUMENTATION
+        echo $OPEN_ROW;
+        drawLabel('DOCUMENTATION', $LISTING_LABEL);
+        drawFileUpload($ADD_ITEM_DOCUMENTATION, $ADD_ITEM_DOCUMENTATION, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
+        echo $CLOSE_ROW;
+        
+        // ROW RECEIPT
+        echo $OPEN_ROW;
+        drawLabel('RECEIPT', $LISTING_LABEL);
+        drawFileUpload($ADD_ITEM_RECEIPT, $ADD_ITEM_RECEIPT, $LISTING_DROPZONE, $ACCEPTED_IMAGE_TYPES);
+        echo $CLOSE_ROW;
+        
+        // ROW PURCHASE DATE
+        echo $OPEN_ROW;
+        drawLabel('PURCHASE DATE', $LISTING_LABEL);
+        drawDateInput($ADD_ITEM_ORIGINAL_PURCHASE_DATE, $LISTING_INPUT_AREA);
+        echo $CLOSE_ROW;
+        
+        // ROW PURCHASE PRICE
+        echo $OPEN_ROW;
+        drawLabel('PURCHASE PRICE', $LISTING_LABEL);
+        drawTextInput($ADD_ITEM_ORIGINAL_PURCHASE_PRICE, $LISTING_INPUT_AREA);
+        echo $CLOSE_ROW;
+        
+        // ROW AFFIDAVIT OF QUALITY
+        echo $OPEN_ROW;
+        drawLabel('AFFIDAVIT OF QUALITY', $LISTING_LABEL);
+        drawAffidavit($ADD_ITEM_AFFIDAVIT_NAME, $ADD_ITEM_AFFIDAVIT_VERIFY, 'i gaurantee that this thing works...');
+        echo $CLOSE_ROW;
+        
+        // ROW SUBMIT BUTTON
+        echo $OPEN_ROW;
+        drawSubmitButton($BLUE_BUTTON." mx-40 my-10 w-full");
+        echo $CLOSE_ROW;
+        
+        // CLOSE FORM
+        echo '</form>';
+
+        return;
+        
+    }
+
 ?>
