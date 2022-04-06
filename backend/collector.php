8a2e58667b7599158f5b875740f1a63532612df2
jrmachado <jrmachado@electro-matic.com>
2022-04-06 12:09:12 -0400

    assembeled data for price algorithm

--- backend/collector.php
+++ backend/collector.php
@@ -1,11 +1,20 @@
 <?php
 
+    include_once('../functions/functions.pricing_algorithm.php');
+    include_once('../constants/constants.all.php');
+
     try{
         switch($GLOBALS['url_loc'][2]){
             case $ADD_ITEM:
                 // virus scan
                 // pass: 
-                //   $item_data = assembleItemData();
+                $price = pricing(
+                    $_POST[$add], 
+                    $_POST[$ADD_ITEM_ORIGINAL_PURCHASE_DATE], 
+                    $PRICE_FLOOR, 
+                    $DAYS_TO_MINIMUM_PIRCE
+                );
+                $item_data = assembleItemData();
                 //   DatabaseConnector::addItem($item_data)
                 //   $no_threats_detected = TRUE; 
                 //   get $user_id;
