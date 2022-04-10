01743d6cba30a4dee3c7beebb066d2fe43f94e96
saltrupiao <strupiano@oakland.edu>
2022-04-10 16:21:34 -0400

    Removed old mysqli logic

--- frontend/admin.php
+++ frontend/admin.php
@@ -1,18 +1,3 @@
-<?php
-
-$serverName = "35.196.103.19";
-$userName = "admin";
-$password = "oakland";
-$dbName = "NFTools";
-
-$conn = mysqli_connect($serverName, $userName, $password, $dbName);
-
-$sqlTable = "SELECT i.i_id, i.i_name, i.owner_id, u.fname, u.lname FROM item i JOIN user u ON i.owner_id = u.id WHERE is_approved=0 AND was_reviewed=0";
-
-$resultTable = $conn->query($sqlTable);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
