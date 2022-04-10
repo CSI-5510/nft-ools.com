a55da2922cc58591c211ff8af165e48a33612e2f
saltrupiao <strupiano@oakland.edu>
2022-04-10 14:38:25 -0400

    Add POST data equation statements

--- backend/adminApprovalController.php
+++ backend/adminApprovalController.php
@@ -10,4 +10,14 @@ $conn = mysqli_connect($serverName, $userName, $password, $dbName);
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $itemIDin = $_POST['itemID'];
     echo "<h1>Item ID: $itemIDin</h1>";
+
+    $itemNameIn = $_POST['itemName'];
+    $itemCategoryIn = $_POST['itemCategory'];
+    $itemSerialNumberIn = $_POST['itemSerialNumber'];
+    $itemDescriptionIn = $_POST['itemDescription'];
+    $itemPurchaseDateIn = $_POST['itemPurchaseDate'];
+    $itemPurchasePriceIn = $_POST['itemPurchasePrice'];
+    $itemAffidavitIn = $_POST['itemAffidavit'];
+    $itemApprovedRadiosIn = $_POST['approvalRadios'];
+    echo "<h1>Approval Decision: $itemApprovedRadiosIn</h1>";
 }
