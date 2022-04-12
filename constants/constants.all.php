73b5fc961861facc6b99f8d095a795036e84bb10
jrmachado <jrmachado@electro-matic.com>
2022-04-11 23:11:49 -0400

    constants now use define()

--- constants/constants.all.php
+++ constants/constants.all.php
@@ -1,66 +1,102 @@
 <?php
 
 
-    // NAVIGATION
-    $ADD_TO_CART = 'add_to_cart';
-	$REMOVE_FROM_CART = 'remove_from_cart';
-
-    $EDIT = 'edit';
-    $ADD_ITEM = 'add_item';
-    $ADD_ITEM_CONFIRMATION = 'add_item_confirmation';
-    $CANCEL_ADD_ITEM = 'cancel_add_item';
+    /*
+        reworked to use define().
+        old versions left in because i wrote ./utilities/constant_conversion.py to do the work.
+        still needs to be validated: 4-11-2022 15:00
+    */
 
 	// ADMIN CONTROLS
 	DEFINE("REVIEW",'review');
-	
-	
+
+    
+    // NAVIGATION
+    // $ADD_TO_CART = 'add_to_cart';
+	// $REMOVE_FROM_CART = 'remove_from_cart';
+    // $EDIT = 'edit';
+    // $ADD_ITEM = 'add_item';
+    // $ADD_ITEM_CONFIRMATION = 'add_item_confirmation';
+    // $CANCEL_ADD_ITEM = 'cancel_add_item';
+    define("ADD_TO_CART","add_to_cart");
+    define("REMOVE_FROM_CART","remove_from_cart");
+    define("EDIT","edit");
+    define("ADD_ITEM","add_item");
+    define("ADD_ITEM_CONFIRMATION","add_item_confirmation");
+    define("CANCEL_ADD_ITEM","cancel_add_item");
+
+
     // FORMATTING
-    $BLUE_BUTTON =  'bg-gray-800 rounded-lg text-gray-100 font-bold text-text-center p-4 m-4 transition duration-300 ease-in-out hover:bg-gray-600';
-    $LISTING_LABEL = 'p-4 m-10 ml-40 mr-5 w-2/5 bg-gray-300 text-2xl font-bold text-center';
-    $CONFIRMATION_LABEL = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2';
-    $LISTING_INPUT_AREA = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-white border-2 border-gray-800 text-center';
-    $LISTING_DROPZONE = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2 border-blue-800 hover:bg-blue-400 text-center';
-    $LISTING_AFFIDAVIT_CONTAINER = '';
-    $LISTING_PRICE_LABEL = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-gray-300';
-    $FLEX_ROW_NO_JUSTIFY = 'flex flex-row items-start';
-    $FLEX_ROW_JUSTIFY = 'flex flex-row justify-between items-start';
-    $CONFIRMATION_IMAGE = 'p-4 m-10 ml-5 mr-40 w-3/5';
+    // $BLUE_BUTTON =  'bg-gray-800 rounded-lg text-gray-100 font-bold text-text-center p-4 m-4 transition duration-300 ease-in-out hover:bg-gray-600';
+    // $LISTING_LABEL = 'p-4 m-10 ml-40 mr-5 w-2/5 bg-gray-300 text-2xl font-bold text-center';
+    // $CONFIRMATION_LABEL = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2';
+    // $LISTING_INPUT_AREA = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-white border-2 border-gray-800 text-center';
+    // $LISTING_DROPZONE = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2 border-blue-800 hover:bg-blue-400 text-center';
+    // $LISTING_AFFIDAVIT_CONTAINER = '';
+    // $LISTING_PRICE_LABEL = 'p-4 m-10 ml-5 mr-40 w-3/5 bg-gray-300';
+    // $FLEX_ROW_NO_JUSTIFY = 'flex flex-row items-start';
+    // $FLEX_ROW_JUSTIFY = 'flex flex-row justify-between items-start';
+    // $CONFIRMATION_IMAGE = 'p-4 m-10 ml-5 mr-40 w-3/5';
+    define("BLUE_BUTTON","bg-gray-800 rounded-lg text-gray-100 font-bold text-text-center p-4 m-4 transition duration-300 ease-in-out hover:bg-gray-600");
+    define("LISTING_LABEL","p-4 m-10 ml-40 mr-5 w-2/5 bg-gray-300 text-2xl font-bold text-center");
+    define("CONFIRMATION_LABEL","p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2");
+    define("LISTING_INPUT_AREA","p-4 m-10 ml-5 mr-40 w-3/5 bg-white border-2 border-gray-800 text-center");
+    define("LISTING_DROPZONE","p-4 m-10 ml-5 mr-40 w-3/5 bg-blue-200 border-2 border-blue-800 hover:bg-blue-400 text-center");
+    define("LISTING_AFFIDAVIT_CONTAINER","");
+    define("LISTING_PRICE_LABEL","p-4 m-10 ml-5 mr-40 w-3/5 bg-gray-300");
+    define("FLEX_ROW_NO_JUSTIFY","flex flex-row items-start");
+    define("FLEX_ROW_JUSTIFY","flex flex-row justify-between items-start");
+    define("CONFIRMATION_IMAGE","p-4 m-10 ml-5 mr-40 w-3/5");
 
 
     // ACCEPTED FILE TYPES
-    $ACCEPTED_IMAGE_TYPES = '.jpg,.jpeg,.png';
+    //$ACCEPTED_IMAGE_TYPES = '.jpg,.jpeg,.png';
+    define("ACCEPTED_IMAGE_TYPES",".jpg,.jpeg,.png");
 
 
     
     // PRICING
-    $DAYS_TO_MINIMUM_PIRCE = 37;
-    $PRICE_FLOOR = 0.1;
+    // $DAYS_TO_MINIMUM_PIRCE = 37;
+    // $PRICE_FLOOR = 0.1;
+    define("DAYS_TO_MINIMUM_PIRCE",37);
+    define("PRICE_FLOOR",0.1);
 
 
     // ADD ITEM POST
-    $ADD_ITEM_TITLE = 'title';
-    $ADD_ITEM_CATEGORY = 'category';
-    $ADD_ITEM_DESCRIPTION = 'description';
-    $ADD_ITEM_IMAGE = 'image';
-    $ADD_ITEM_DOCUMENTATION = 'documentation';
-    $ADD_ITEM_RECEIPT = 'receipt';
-    $ADD_ITEM_ORIGINAL_PURCHASE_DATE = 'original_purchase_date';
-    $ADD_ITEM_ORIGINAL_PURCHASE_PRICE = 'original_price';
-    $ADD_ITEM_AFFIDAVIT_NAME = 'affidavit_name';
-    $ADD_ITEM_AFFIDAVIT_VERIFY = 'affidavit_verify';
-    $ADD_ITEM_SERIAL_NUMBER = 'serial';
+    // $ADD_ITEM_TITLE = 'title';
+    // $ADD_ITEM_CATEGORY = 'category';
+    // $ADD_ITEM_DESCRIPTION = 'description';
+    // $ADD_ITEM_IMAGE = 'image';
+    // $ADD_ITEM_DOCUMENTATION = 'documentation';
+    // $ADD_ITEM_RECEIPT = 'receipt';
+    // $ADD_ITEM_ORIGINAL_PURCHASE_DATE = 'original_purchase_date';
+    // $ADD_ITEM_ORIGINAL_PURCHASE_PRICE = 'original_price';
+    // $ADD_ITEM_AFFIDAVIT_NAME = 'affidavit_name';
+    // $ADD_ITEM_AFFIDAVIT_VERIFY = 'affidavit_verify';
+    // $ADD_ITEM_SERIAL_NUMBER = 'serial';
+    define("ADD_ITEM_TITLE","title");
+    define("ADD_ITEM_CATEGORY","category");
+    define("ADD_ITEM_DESCRIPTION","description");
+    define("ADD_ITEM_IMAGE","image");
+    define("ADD_ITEM_DOCUMENTATION","documentation");
+    define("ADD_ITEM_RECEIPT","receipt");
+    define("ADD_ITEM_ORIGINAL_PURCHASE_DATE","original_purchase_date");
+    define("ADD_ITEM_ORIGINAL_PURCHASE_PRICE","original_price");
+    define("ADD_ITEM_AFFIDAVIT_NAME","affidavit_name");
+    define("ADD_ITEM_AFFIDAVIT_VERIFY","affidavit_verify");
+    define("ADD_ITEM_SERIAL_NUMBER","serial");
 
 
     // ADD ITEM QUERY
-    $ADD_ITEM_QUERY_NAME = 'i_name';
-    $ADD_ITEM_QUERY_DESCRIPTION = 'i_description';
-    $ADD_ITEM_QUERY_CURRENT_PRICE = 'current_price';
-    $ADD_ITEM_QUERY_IMAGE = 'i_image';
-    $ADD_ITEM_QUERY_CATEGORY = 'i_category_Id';
-    $ADD_ITEM_QUERY_SERIAL_NUMBER = 'i_serialnum';
-    $ADD_ITEM_QUERY_ORIGINAL_PURCHASE_PRICE = 'original_price';
-    $ADD_ITEM_QUERY_ORIGINAL_PURCHASE_DATE = 'original_purchase_date';
-    $ADD_ITEM_QUERY_DAYS_TO_MINIMUM_PRICE = 'days_to_minimum_price';
+    define("ADD_ITEM_QUERY_NAME", 'i_name');
+    define("ADD_ITEM_QUERY_DESCRIPTION", "i_description");
+    define("ADD_ITEM_QUERY_CURRENT_PRICE","current_price");
+    define("ADD_ITEM_QUERY_IMAGE","i_image");
+    define("ADD_ITEM_QUERY_CATEGORY","i_category_Id");
+    define("ADD_ITEM_QUERY_SERIAL_NUMBER","i_serialnum");
+    define("ADD_ITEM_QUERY_ORIGINAL_PURCHASE_PRICE","original_price");
+    define("ADD_ITEM_QUERY_ORIGINAL_PURCHASE_DATE","original_purchase_date");
+    define("ADD_ITEM_QUERY_DAYS_TO_MINIMUM_PRICE","days_to_minimum_price");
 
     
 ?>
