8d0f85923bfae297f1b0b84571f07ead367d2f31
Pooja Thote <93176568+shreja19@users.noreply.github.com>
2022-04-15 18:19:49 -0400

    adding about-us page

---
+++ about-us.php
@@ -0,0 +1,52 @@
+<html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style2.css">
+    <title>NFTools</title>
+ </head>
+<body>
+<div class="bg-purple-800 p-9">
+  <div class="bg-white flex flex-col font-sans">	
+   <div class="container mx-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
+      <div class="max-h-70 md:h-screen">
+        <img class="w-screen h-screen" src="nftools.jpg" >
+      </div>
+      <div class="flex bg-gray-100 p-10">
+        <div class="mb-auto mt-auto max-w-lg">
+          <h1 class="text-4xl uppercase text-blue-900">About Us</h1>
+          <p class="font-semibold mb-5"></p>
+          <h3 class="text-2xl text-gray-500">Background-: </h3>
+          <p>We need tools everyday, and tools are expensive whether it may be purchasing & renting – either way, the buyer is going to be paying a premium.Determining the lineage of usage/wear on a tool is almost impossible to do so, as these records are not consistently collected. </p>
+          <p>The solution is NFTools creating a platform that allows users to resell their tools close to the purchase price, Creating an easy way to determine the lineage of ownership of a tool to guarantee its value.Our Tool Exchange Platform would Validate the Chain of Ownership (lineage) of a particular tool and provides functionalities for selling and purchasing tools. </p>
+          <h3 class="text-2xl text-gray-500">Features Of Our Platform-: </h3>
+          <p>Provides assurance of tool quality and value via Chain of Ownership tracking, Users can resell unwanted tools at very close to the purchase price (e.g. 95%), depending on usage; as opposed to renting or trading it in, Built-in payment system for peer-to-peer payments for transfer of ownership transactions.Single Platform for both the purchase and procurement of tools with verified tracking of how long the items have been owned.Dynamic searching features allow user to see a refined list of either items or visual representations of tool categories based on the search query.
+
+
+          </p>
+          <h3 class="text-2xl text-gray-500">Meet Our Development Team-:</h3>
+          <p>1. Daiyaan Ijaz</p>
+          <p>2. John Machado</p>
+          <p>3. Pooja Thote</p>
+          <p>4. Sal Trupiano</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
+</html>
