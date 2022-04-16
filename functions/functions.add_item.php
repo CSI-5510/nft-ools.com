4b4799be00db7781f7c2fe2cc2e5f5b9e47c97b1
jrmachado <jrmachado@electro-matic.com>
2022-04-16 13:52:23 -0400

    committing to merge poojas changes

--- functions/functions.add_item.php
+++ functions/functions.add_item.php
@@ -44,7 +44,7 @@
         // ROW TITLE
         echo $OPEN_ROW;
         echo drawLabel('TITLE', LISTING_LABEL);
-        echo drawTextInput(ITEM_QUERY_NAME, LISTING_INPUT_AREA, 20, TRUE);
+        echo drawTextInput(ITEM_QUERY_NAME, LISTING_INPUT_AREA, 45, TRUE);
         echo $CLOSE_ROW;
         
         // ROW CATEGORY
@@ -56,13 +56,13 @@
         // ROW SERIAL NUMBER
         echo $OPEN_ROW;
         echo drawLabel('SERIAL NUMBER', LISTING_LABEL);
-        echo drawTextInput(ITEM_QUERY_SERIAL_NUMBER, LISTING_INPUT_AREA, 20, TRUE);
+        echo drawTextInput(ITEM_QUERY_SERIAL_NUMBER, LISTING_INPUT_AREA, 11, TRUE);
         echo $CLOSE_ROW;
 
         // ROW DESCRIPTION
         echo $OPEN_ROW;
         echo drawLabel('DESCRIPTION', LISTING_LABEL);
-        echo drawTextArea(ITEM_QUERY_DESCRIPTION, LISTING_INPUT_AREA);
+        echo drawTextArea(ITEM_QUERY_DESCRIPTION, LISTING_INPUT_AREA, 5, TRUE);
         echo $CLOSE_ROW;
         
         // ROW IMAGE
