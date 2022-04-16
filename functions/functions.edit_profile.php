4fe8ba86ec293faebc7f7ea755b4c6970a871bf7
jrmachado <jrmachado@electro-matic.com>
2022-04-16 14:28:54 -0400

    changed draw function calls in functions.edit_profile.php to echot + call

--- functions/functions.edit_profile.php
+++ functions/functions.edit_profile.php
@@ -18,14 +18,14 @@
         
         // ROW USERNAME
         echo $OPEN_ROW;
-        drawLabel('UserName', LISTING_LABEL);
-        drawTextInput(USER_TABLE_USERNAME, LISTING_INPUT_AREA, 20, TRUE, $data[USER_TABLE_USERNAME]);
+        echo drawLabel('UserName', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_USERNAME, LISTING_INPUT_AREA, 20, TRUE, $data[USER_TABLE_USERNAME]);
         echo $CLOSE_ROW;
         
         // ROW EMAIL
         echo $OPEN_ROW;
-        drawLabel('Email', LISTING_LABEL);
-        drawTextInput(USER_TABLE_EMAIL, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_EMAIL]);
+        echo drawLabel('Email', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_EMAIL, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_EMAIL]);
         echo $CLOSE_ROW;
         
         // ROW PASSWORD
@@ -36,55 +36,55 @@
 
         // ROW FIRST NAME
         echo $OPEN_ROW;
-        drawLabel('First Name', LISTING_LABEL);
-        drawTextInput(USER_TABLE_FIRST_NAME, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_FIRST_NAME]);
+        echo drawLabel('First Name', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_FIRST_NAME, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_FIRST_NAME]);
         echo $CLOSE_ROW;
         
         // ROW LAST NAME
         echo $OPEN_ROW;
-        drawLabel('Last Name', LISTING_LABEL);
-        drawTextInput(USER_TABLE_LAST_NAME, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_LAST_NAME]);
+        echo drawLabel('Last Name', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_LAST_NAME, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_LAST_NAME]);
         echo $CLOSE_ROW;
         
         // ROW ADDRESS LINE 1
         echo $OPEN_ROW;
-        drawLabel('Address Line 1', LISTING_LABEL);
-        drawTextInput(USER_TABLE_ADDR_LINE_1, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ADDR_LINE_1]);
+        echo drawLabel('Address Line 1', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_ADDR_LINE_1, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ADDR_LINE_1]);
         echo $CLOSE_ROW;
         
         // ROW ADDRESS LINE 2
         echo $OPEN_ROW;
-        drawLabel('Address Line 2', LISTING_LABEL);
-        drawTextInput(USER_TABLE_ADDR_LINE_2, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ADDR_LINE_2]);
+        echo drawLabel('Address Line 2', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_ADDR_LINE_2, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ADDR_LINE_2]);
         echo $CLOSE_ROW;
         
         // ROW CITY
         echo $OPEN_ROW;
-        drawLabel('City', LISTING_LABEL);
-        drawTextInput(USER_TABLE_CITY, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_CITY]);
+        echo drawLabel('City', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_CITY, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_CITY]);
         echo $CLOSE_ROW;
         
         // ROW STATE
         echo $OPEN_ROW;
-        drawLabel('State', LISTING_LABEL);
-        drawTextInput(USER_TABLE_STATE, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_STATE]);
+        echo drawLabel('State', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_STATE, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_STATE]);
         echo $CLOSE_ROW;
         
         // ROW ZIP CODE
         echo $OPEN_ROW;
-        drawLabel('Zip Code', LISTING_LABEL);
-        drawTextInput(USER_TABLE_ZIP, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ZIP]);
+        echo drawLabel('Zip Code', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_ZIP, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_ZIP]);
         echo $CLOSE_ROW;
 		
 		// ROW PHONE NUMBER
         echo $OPEN_ROW;
-        drawLabel('Phone Number', LISTING_LABEL);
-        drawTextInput(USER_TABLE_PHONE, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_PHONE]);
+        echo drawLabel('Phone Number', LISTING_LABEL);
+        echo drawTextInput(USER_TABLE_PHONE, LISTING_INPUT_AREA, 20, TRUE,$data[USER_TABLE_PHONE]);
         echo $CLOSE_ROW;
 		
         // ROW SUBMIT BUTTON
         echo $OPEN_ROW;
-        drawSubmitButton(BLUE_BUTTON." mx-40 my-10 w-full");
+        echo drawSubmitButton(BLUE_BUTTON." mx-40 my-10 w-full");
         echo $CLOSE_ROW;
         
         // CLOSE FORM
