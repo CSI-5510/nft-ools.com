2c2e136dcb6131d2d654e4ce6bc3dff900588afc
jrmachado <jrmachado@electro-matic.com>
2022-04-18 11:31:30 -0400

    search working

--- frontend/search.php
+++ frontend/search.php
@@ -1,11 +1,12 @@
 <?php
 
-    if(is_null($item_list)){
+    if(empty($items_list)){
         echo "no entries found";
+        return;
     }
     foreach ($items_list as $item){
         echo drawItemRow($item);
     }
 
-
+    
 ?>
