006bbee6924b3a1f484773655c28efa4a8b389a0
jrmachado <jrmachado@electro-matic.com>
2022-04-17 21:25:55 -0400

    template for required fields marking complete

--- backend/login.php
+++ backend/login.php
@@ -20,7 +20,9 @@ if (DatabaseConnector::query('SELECT email from user WHERE email=:email', array(
 	if(password_verify($password, DatabaseConnector::query('SELECT password from user WHERE email=:email', array(':email'=>$emailoruser))[0]['password'])){
 		$success = 1;
 		$cstrong = True;
-		$domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;		
+		// $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+		$domain = /*$protocol . */ $_SERVER['HTTP_HOST'] .'/'. $GLOBALS["url_loc"][0]; 
+		var_dump($domain);		
 		$token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
 		$user_id = DatabaseConnector::query('SELECT id from user WHERE email=:email', array(':email'=>$emailoruser))[0]['id'];
 		DatabaseConnector::query('INSERT INTO login_tokens (token, user_id) VALUES (:token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
