c715026618f809457851c9394977473ccbd2040b
saltrupiao <strupiano@oakland.edu>
2022-04-18 20:24:10 -0400

    remove OO profile aside, add conventional

--- frontend/aside.php
+++ frontend/aside.php
@@ -119,11 +119,34 @@
          echo '<br>';
 		 echo showAsideAddItem(USER_ID);
          echo showAsideMyItems(USER_ID);
-         echo showAsideProfile(USER_ID);
+         //echo showAsideProfile(USER_ID);
          echo '<br>';
       ?>
 
-     <?php endif; ?>  
+
+          <li class="my-px">
+              <a href="<?php if($GLOBALS['config']['url_root']!== ""){echo $GLOBALS['config']['url_root'];echo"/";} echo $GLOBALS["url_loc"][0]; ?>/profile" class="<?php echo $GLOBALS["url_loc"][1] !==
+              "Profile"
+                  ? "flex flex-row items-center h-10 px-3 rounded-lg text-gray-500 hover:bg-gray-100 hover:text-gray-700 transition"
+                  : "flex flex-row items-center h-10 px-3 rounded-lg text-gray-600 bg-gray-100 hover:bg-gray-100 hover:text-gray-700"; ?>">
+               <span class="flex items-center justify-center text-lg text-gray-400">
+                  <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6" fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="2"
+                  >
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+               </span>
+                  <span class="ml-3">
+               Profile
+               </span>
+              </a>
+          </li>
+
+      <?php endif; ?>
 	 
 	 
 
