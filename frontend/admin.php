3806e22fef6a54f946a09277ebe8c2a332268a3a
saltrupiao <strupiano@oakland.edu>
2022-04-18 21:05:49 -0400

    change URL Loc

--- frontend/admin.php
+++ frontend/admin.php
@@ -1,5 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<?php if(!isset($GLOBALS['url_loc'][3])): ?>
+<?php if(!isset($GLOBALS['url_loc'][2])): ?>
 <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">&nbsp;NFT-ools Admin</a>
