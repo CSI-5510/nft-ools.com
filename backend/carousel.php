5b4fc79d02f400f89150ece6d2a6b6def1b35164
saltrupiao <strupiano@oakland.edu>
2022-04-18 23:37:24 -0400

    echo category_tiles_data

--- backend/carousel.php
+++ backend/carousel.php
@@ -1,6 +1,7 @@
 <?php
 
     $category_tiles_data = DatabaseConnector::getCategoryCarouselData();
+    var_dump($category_tiles_data);
     // var_dump(DatabaseConnector::getLastItemAddedByUser());
 
 ?> 
