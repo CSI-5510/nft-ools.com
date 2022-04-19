91b9dfc1083f7d42a1e48139383b742098d67111
saltrupiao <strupiano@oakland.edu>
2022-04-18 23:39:53 -0400

    add var dumps

--- backend/carousel.php
+++ backend/carousel.php
@@ -2,6 +2,9 @@
 
     $category_tiles_data = DatabaseConnector::getCategoryCarouselData();
     var_dump($category_tiles_data);
+    foreach ($category_tiles_data as $r) {
+        var_dump($r[CATEGORY_TABLE_ID]);
+    }
     // var_dump(DatabaseConnector::getLastItemAddedByUser());
 
 ?> 
