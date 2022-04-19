f7d15a64c39380f866ad86696005d46ec20429fc
saltrupiao <strupiano@oakland.edu>
2022-04-18 23:50:40 -0400

    change to LOC_1 - for production server 2

--- backend/category.php
+++ backend/category.php
@@ -5,7 +5,7 @@
 
     switch($item_id){
         case is_numeric($item_id):
-			$items_list = DatabaseConnector::getItemsData(URL_LOC_2);	
+			$items_list = DatabaseConnector::getItemsData($item_id);
             break;
         case URL_USER:
             if(!USER_ID){
