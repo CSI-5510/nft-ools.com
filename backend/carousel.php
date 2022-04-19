7a34c591244b494c71010d963f25ef85d089373c
saltrupiao <strupiano@oakland.edu>
2022-04-18 23:40:08 -0400

    remove first var dump

--- backend/carousel.php
+++ backend/carousel.php
@@ -1,7 +1,6 @@
 <?php
 
     $category_tiles_data = DatabaseConnector::getCategoryCarouselData();
-    var_dump($category_tiles_data);
     foreach ($category_tiles_data as $r) {
         var_dump($r[CATEGORY_TABLE_ID]);
     }
