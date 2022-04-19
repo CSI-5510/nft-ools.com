618b2ceb565c1fc396c7493dd53ffd9aebe00a2a
Pooja <pthote@oakland.edu>
2022-04-19 14:34:28 -0700

    modify query

--- classes/class.admin.php
+++ classes/class.admin.php
@@ -41,7 +41,7 @@
             #$userid = User::isLoggedIn();
 
 		    $q1='SELECT owner_id from item where i_id='.$itemId;
-            $userid=DatabaseConnector::query($q1);
+            $userid=DatabaseConnector::query($q1)[0];
             $q='INSERT INTO message (msg_id,uid,item_id,message_body,approval_timestamp,is_acknowledged) VALUES 
             (NULL,'.$userid.','.$itemId.',"Dear User-Your item listing request was approved by NFTools Admin and check your account to know more details",CURRENT_TIMESTAMP,0)';
             var_dump($q);
