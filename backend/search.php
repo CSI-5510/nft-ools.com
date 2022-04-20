24d02ec0e94960300693cfe91048ee2aabc356c0
jrmachado <jrmachado@electro-matic.com>
2022-04-20 13:21:54 -0400

    keyword search fixed

--- backend/search.php
+++ backend/search.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $key_word = str_replace('%20', ' ', URL_LOC_2);
+    $key_word = str_replace('%20', ' ', URL_LOC_1);
     $items_list = DatabaseConnector::keywordSearch($key_word);
 
 
