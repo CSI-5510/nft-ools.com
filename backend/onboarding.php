b4524ec6be4974397c727e1c99fb11fccd44a0bb
saltrupiao <strupiano@oakland.edu>
2022-04-19 22:06:57 -0400

    remove validation for setup

--- backend/onboarding.php
+++ backend/onboarding.php
@@ -1,4 +1,5 @@
 <?php
+/*
 //use this function in the user class to see if the user is logged in
 if (!User::isLoggedin()){
 	header("Location: ./login");
@@ -7,8 +8,9 @@ $userid = User::isLoggedIn();
 
 if (User::getUsername($userid)){
 	//redirect mans if hes got a username already
-	header("Location: ./home");	
+	header("Location: ./home");
 }
+*/
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {	
 
