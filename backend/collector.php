d5f5f6ee935e23d9d4c3602c956247a8e9cec3ed
jrmachado <jrmachado@electro-matic.com>
2022-04-19 21:38:37 -0400

    fixing sell item button

--- backend/collector.php
+++ backend/collector.php
@@ -14,7 +14,7 @@
     try{
         switch(URL_LOC_1){
             case URL_ADD_EVENT:
-                $item_id = URL_LOC_2;
+                $item_id = URL_LOC_1;
                 $event_data = insertEventFormReducer($item_id, $_POST);
                 insertEvent($event_data);
                 break;
