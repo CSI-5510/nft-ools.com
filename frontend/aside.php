a3403b834dc57ef4083378b4a36b4086c9264554
Pooja <pthote@oakland.edu>
2022-04-20 09:23:53 -0700

    deleted stuff related to messages button

--- frontend/aside.php
+++ frontend/aside.php
@@ -165,9 +165,7 @@
                   <span class="ml-3">
                My Pending Items
                </span>
-              // <span class="flex items-center justify-center text-xs text-red-500 font-semibold bg-red-100 h-6 px-2 rounded-full ml-auto">
-               //   <?php echo User::getAllMessagesCount(); ?>
-              // </span>
+              
               </a>
           </li>
 
