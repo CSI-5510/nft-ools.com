13374dc73674486885a7d8a5f7f28c8c6917edc8
saltrupiao <strupiano@oakland.edu>
2022-04-19 22:36:56 -0400

    rollback 2

--- backend/onboarding.php
+++ backend/onboarding.php
@@ -1,18 +1,16 @@
 <?php
-
 /*
 //use this function in the user class to see if the user is logged in
 if (!User::isLoggedin()){
-    header("Location: ./login");
+	header("Location: ./login");
 }
 $userid = User::isLoggedIn();
-
 if (User::getUsername($userid)){
-    //redirect mans if hes got a username already
-    header("Location: ./home");
+	//redirect mans if hes got a username already
+	header("Location: ./home");
 }
-
 */
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
@@ -23,9 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $address = $_POST['address'];
     $city = $_POST['city'];
     $zipcode = $_POST['zipcode'];
-    $state = $_POST['state'];
-    $phone = $_POST['phone'];
-    $address2 = $_POST['address2'];
+
     try{
 //throw an error if you didn't fill input in or if input value is empty
         if(!isset($_POST['fname']) || $_POST['fname'] === ""){ throw new Exception('Error: You need to input all fields!'); }
@@ -36,18 +32,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if(!isset($_POST['zipcode']) || $_POST['zipcode'] === ""){ throw new Exception('Error: You need to input all fields!'); }
 
 
-        if(!isset($_POST['phone']) || $_POST['address'] === ""){ throw new Exception('Error: You need to input all fields!'); }
-        if(!isset($_POST['address2']) || $_POST['city'] === ""){ throw new Exception('Error: You need to input all fields!'); }
-        if(!isset($_POST['state']) || $_POST['zipcode'] === ""){ throw new Exception('Error: You need to input all fields!'); }
-
 
         if(!is_numeric($zipcode)){ throw new Exception('Error: Zip code must be a number!'); }
-        if(User::isUsernameTaken($username)){ throw new Exception('Error: Username is already taken!'); }
 
-        DatabaseConnector::query('UPDATE user SET username=:username, fname=:fname, lname=:lname,addr_line_1=:address,city=:city,zip=:zipcode,addr_line_2=:address2,phone=:phone,state=:state WHERE id=:userid', array(':username'=>$username,':fname'=>$fname,':lname'=>$lname,':address'=>$address, ':userid'=>$userid, ':city'=>$city,':zipcode'=>$zipcode,':address2'=>$address2,':phone'=>$phone,':state'=>$state));
+        DatabaseConnector::query('UPDATE user SET username=:username, fname=:fname, lname=:lname,addr_line_1=:address,city=:city,zip=:zipcode WHERE id=:userid', array(':username'=>$username,':fname'=>$fname,':lname'=>$lname,':address'=>$address, ':userid'=>$userid, ':city'=>$city,':zipcode'=>$zipcode));
 
         $success =true;
-    }    catch (Exception $e) {
+    }	catch (Exception $e) {
         $GLOBALS['errors'][] = $e->getMessage();
     }
 }
