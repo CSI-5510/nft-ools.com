90816fb27301035ade021212f0ad1702e4ffcdd1
jrmachado <jrmachado@electro-matic.com>
2022-04-19 20:50:20 -0400

    wrong variable name

--- functions/functions.item.php
+++ functions/functions.item.php
@@ -289,7 +289,7 @@
 
     function decideSellButton($item_id){
         $item_data = DatabaseConnector::getItemDataNoPics($item_id);
-        var_dump($item_data[ITEM_OBFUSCATED_OWNER_ID]);
+        var_dump($item_data[ITEM_TABLE_OWNER_ID]);
         echo "<br><br>";
         var_dump(USER_ID);
         if(intval($item_data[ITEM_TABLE_OWNER_ID])!==USER_ID){
