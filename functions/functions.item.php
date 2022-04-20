46d0f8c70ae1bc7701abc7d1f2e46d660daeffad
jrmachado <jrmachado@electro-matic.com>
2022-04-19 21:09:09 -0400

    fixing sell item button

--- functions/functions.item.php
+++ functions/functions.item.php
@@ -293,12 +293,12 @@
             return drawBlank();
         }
         if(!$item_data[ITEM_TABLE_IS_APPROVED]){
-            var_dump($item_data[ITEM_TABLE_OWNER_ID]);
-            echo "<br><br>";
-            var_dump(USER_ID);
             return drawBlank();
         }
         if($item_data[ITEM_TABLE_REJECTED]){
+            var_dump($item_data[ITEM_TABLE_OWNER_ID]);
+            echo "<br><br>";
+            var_dump(USER_ID);
             return drawBlank();
         }
         if($item_data[ITEM_TABLE_IN_CART]){
